1cf0ddebcce00ac12313110092b4bc494325c2d6
Vivek_SIMCA <122796418+tqvivek@users.noreply.github.com>
2025-12-18 22:22:20 +0530

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New Enquiry From Koshank Connect Page"; //letter subject
+	$to = 'shankardange@yahoo.com'; // Enter Your E-mail
+	$from = 'admin@koshank.com'; // Admin e-mail
+	$fromName = 'Koshank'; // Your company name
+	$charset = 'UTF-8';
+?>
